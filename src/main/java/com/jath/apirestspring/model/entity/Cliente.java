8792a933d4d85c1b156c04fe52d5d79c5198336f
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-04 09:13:02 -0600

    Entidad Cliente

---
+++ src/main/java/com/jath/apirestspring/model/entity/Cliente.java
@@ -0,0 +1,28 @@
+package com.jath.apirestspring.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private Date fechaRegistro;
+}
