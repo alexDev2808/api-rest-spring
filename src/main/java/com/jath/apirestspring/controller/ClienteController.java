a179e72c1ec98e95cd55c22b3c32e855daabf74c
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-04 14:32:34 -0600

    Personalizando validacion de los response

--- src/main/java/com/jath/apirestspring/controller/ClienteController.java
+++ src/main/java/com/jath/apirestspring/controller/ClienteController.java
@@ -3,8 +3,14 @@ package com.jath.apirestspring.controller;
 import com.jath.apirestspring.model.entity.Cliente;
 import com.jath.apirestspring.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -26,10 +32,19 @@ public class ClienteController {
     }
 
     @DeleteMapping("cliente/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id) {
-        Cliente clienteDelete = clienteService.findById(id);
-        clienteService.delete(clienteDelete);
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            Cliente clienteDelete = clienteService.findById(id);
+            clienteService.delete(clienteDelete);
+            return new ResponseEntity<>(clienteDelete, HttpStatus.NO_CONTENT);
+        } catch (DataAccessException e) {
+            response.put("mensaje", e.getMessage());
+            response.put("cliente", null);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("cliente/{id}")
