10af470e3b8a1fb08f16ee479cc979306d87a40c
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-04 09:01:17 -0600

    Creando proyecto y conexion a la DB

---
+++ src/test/java/com/jath/apirestspring/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jath.apirestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
